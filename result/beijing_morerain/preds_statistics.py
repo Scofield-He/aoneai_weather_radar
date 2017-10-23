f0b9766bf924dc74058f354387c4ebb12edd6b54
root <root@ml.novalocal>
2017-10-23 20:32:22 +0800

    add the result of beijing_data_morerain

---
+++ result/beijing_morerain/preds_statistics.py
@@ -0,0 +1,72 @@
+#! python3
+# coding = utf-8
+import numpy as np
+
+file_path = "/data/yuyang/weather/result/beijing_morerain/"
+
+file_time = "2017-10-19-22"
+gru = "bi-gru_pre-{}.csv".format(file_time)
+rf = "rf_pre-{}.csv".format(file_time)
+xgb = "xgb_pre-{}.csv".format(file_time)
+ensemble = "ensemble-{}.csv".format(file_time)
+label = "label_test.txt"
+
+preds_file = file_path + gru
+label_file = file_path + label
+print(preds_file)
+
+preds = []
+with open(preds_file) as fp:
+    for line in fp:
+        if line:
+            preds.append(float(line.strip()))
+
+labels = []
+with open(label_file) as fl:
+    for line in fl:
+        if line:
+            labels.append(int(line.strip()))
+labels = labels[:len(preds)]
+
+print('length of preds: ', len(preds))
+
+for i,j in zip(labels, preds):
+    print(' {:3d}   {:5.2f}'.format(i, j))
+
+cnt_0, cnt_0_10, cnt_10_20, cnt_20_50, cnt_50_100, cnt_100 = 0, 0, 0, 0, 0, 0
+
+for i in preds:
+    if i == 0:
+        cnt_0 += 1
+    elif 0 < i <= 10:
+        cnt_0_10 += 1
+    elif 10 < i <= 20:
+        cnt_10_20 += 1
+    elif 20 < i <= 50:
+        cnt_20_50 += 1
+    elif 50 < i <= 100:
+        cnt_50_100 += 1
+    else:
+        cnt_100 += 1
+
+print('preds range  count')
+print('     0        {} '.format(cnt_0))
+print('    0-10      {} '.format(cnt_0_10))
+print('   10-20      {} '.format(cnt_10_20))
+print('   20-50      {} '.format(cnt_20_50))
+print('    100       {} '.format(cnt_100))
+
+
+'''
+def rmse(y, x):
+    n = len(x)
+    variances = list(map(lambda x_target, y_prediction: (x_target - y_prediction) ** 2, x, y))
+    return np.sqrt(np.sum(variances) / float(n))
+
+
+for i in range(0, 30, 3):
+    value_assumed = i
+    preds_assumed = [value_assumed] * 10000
+
+    print("rmse == {:.2f} when preds_assumed == {}".format(rmse(label, preds_assumed), value_assumed))
+'''
